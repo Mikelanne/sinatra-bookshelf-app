e407e56e3de9cdbbd1778b259f29865b45a148d4
Mikelanne <mikelanne.wood@gmail.com>
2020-12-18 16:57:43 -0500

    creating form for a user to sign up, logging the user in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,10 @@ class UsersController < ApplicationController
         erb :"users/new"
     end 
 
+    post "/signup" do
+        user = User.create(params[:user])
+        session[:user_id] = user.id 
+        redirect '/books'
+    end 
 
 end 
