b12e9e6f27bf31b371bc73dd67e999d1ada384ca
Mikelanne <mikelanne.wood@gmail.com>
2020-12-19 14:49:37 -0500

    creating a show page for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
 
 
-    get '/books' do #index that loads all the books
+    get '/books' do #need to figure out how to get this to just the user's books
         @books = Book.all
         erb :"books/index"
+    end 
+
+
+    get '/books/:id' do #show page for each book
+        @book = Book.find_by(id: params[:id])
+        erb :"books/show"
     end 
 
 
