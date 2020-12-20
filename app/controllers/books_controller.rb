00aba9987c2a24644287ed7ea694b2a88e5b7bbb
Mikelanne <mikelanne.wood@gmail.com>
2020-12-20 16:12:42 -0500

    displaying only the books belonging to the current user.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
 
 
-    get '/books' do #need to figure out how to get this to just the user's books
-        @books = Book.all
+    get '/books' do
+        @books = current_user.books
         erb :"books/index"
     end 
 
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     end
 
     post '/books/new' do 
-        book = Book.create(params[:book])
+        book = current_user.books.build(params[:book])
         redirect "/books"
     end 
 
